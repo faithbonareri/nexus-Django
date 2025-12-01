4415d418467e0384ea6938442a4a6cff2167413d
faithbonareri <faithbonareri97@gmail.com>
2025-12-01 10:27:05 +0300

    Creating a model and making migrations

--- projectNexus/firstApp/admin.py
+++ projectNexus/firstApp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Shoes, Bags, Clothes
 
 # Register your models here.
+admin.site.register(Shoes)
+admin.site.register(Bags)
+admin.site.register(Clothes)
