6d46f31dc9c5e32a3805011a69ac29e398cec569
Daniel Mwago <0neeyed.folktale@gmail.com>
2025-12-01 14:43:30 +0300

    implement create operation using ModelForms

--- projectNexus/firstApp/urls.py
+++ projectNexus/firstApp/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.nexusHome, name='nexusHome'),
+    path('', views.home, name='home'),
+    path('market/', views.market, name='market'),
+    path('create-shoe/', views.createShoe, name='createShoe'), # New Route
 ]
--- projectNexus/firstApp/views.py
+++ projectNexus/firstApp/views.py
@@ -1,9 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect # Import redirect
 from .models import Shoes
+from .forms import ShoeForm # Import the form
 
-def nexusHome(request):
-    # ORM: "Select * from Shoes"
-    shoes = Shoes.objects.all() 
-    # Context: Passing data from Python to HTML
+def home(request):
+    return render(request, 'firstApp/home.html')
+
+def market(request):
+    shoes = Shoes.objects.all()
     context = {'shoes': shoes}
-    return render(request, 'firstApp/home.html', context)
+    return render(request, 'firstApp/market.html', context)
+
+def createShoe(request):
+    form = ShoeForm()
+    
+    # Check if the user is sending data (POST) or just visiting (GET)
+    if request.method == 'POST':
+        form = ShoeForm(request.POST)
+        if form.is_valid():
+            form.save() 
+            return redirect('market') # Redirect to the market list
+
+    context = {'form': form}
+    return render(request, 'firstApp/form.html', context)
